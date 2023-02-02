b88852ecd4d4ad40361710221dd5c89ea6dc6d02
Julen <juliantxo14@gmail.com>
2023-02-02 16:08:07 +0100

    Solved figure position update and implemented basic movements

--- src/Figure.hpp
+++ src/Figure.hpp
@@ -15,12 +15,23 @@ public:
     Figure(Color color, Type type, const std::pair<int, int> & pos);
     virtual ~Figure(){}
     //virtual std::vector<std::pair<int, int>> getPossibleMovements() = 0;
+    virtual void updatePosition(int x, int y);
 
     //TODO ver si es virtual
     virtual std::shared_ptr<Sprite> getSprite();
 
+
 protected:
 
+    virtual bool positionExist(const std::pair<int, int>  &position);
+    virtual std::pair<int, int> getUpPosition();
+    virtual std::pair<int, int> getDownPosition();
+    virtual std::pair<int, int> getLeftPosition();
+    virtual std::pair<int, int> getRightPosition();
+    virtual std::pair<int, int> getDiagonalUpLeft();
+    virtual std::pair<int, int> getDiagonalUpRight();
+    virtual std::pair<int, int> getDiagonalDownLeft();
+    virtual std::pair<int, int> getDiagonalDownRight();
     Texture figureTexture;
     std::shared_ptr<Sprite> sprite{nullptr};
     std::pair<int, int> position;
