113647fa8b0e1b893a3630c3564b46128f0bd762
Julen <juliantxo14@gmail.com>
2023-02-08 02:37:03 +0100

    Created Pawn funct to know if is initial position

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -3,3 +3,16 @@
 Pawn::Pawn(Color color, const std::pair<int, int> & pos):
 Figure::Figure(color, Type::PAWN, pos)
 {}
+
+bool Pawn::isStartPosition()
+{
+    if(color == Color::BLACK)
+    {
+        if(position.second == 1)
+            return true;
+        return false;
+    }
+    if(position.second == 6)
+        return true;
+    return false;
+}
