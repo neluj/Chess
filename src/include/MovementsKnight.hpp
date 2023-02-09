3c2e653da410a6120d5a6d86371c2cc53ba411ef
Julen <juliantxo14@gmail.com>
2023-02-09 01:30:52 +0100

    Fixed Knight Movement

--- src/include/MovementsKnight.hpp
+++ src/include/MovementsKnight.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include <vector>
 #include "MovementsPositionState.hpp"
+#include "Knight.hpp"
 
 
 class MovementsKnight
 {
 public:
     //Returns figure if the movements ends withempassant
-    std::vector<std::pair<int,int>> getMovements(const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
+    std::vector<std::pair<int,int>> getMovements(const std::shared_ptr<Knight> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
 private:
     MovementsPositionState positionState;
 };
