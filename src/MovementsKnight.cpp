c5e6f287d8633a2ece9b337cf442e2a31a533ef3
Julen <juliantxo14@gmail.com>
2023-02-15 03:05:47 +0100

    Implemented knight movements

--- src/MovementsKnight.cpp
+++ src/MovementsKnight.cpp
@@ -1,4 +1,12 @@
 #include "MovementsKnight.hpp"
+#include "MovementsPositionState.hpp"
+#include "Knight.hpp"
+
+MovementsKnight::MovementsKnight()
+{
+    movementsPositionState = std::make_shared<MovementsPositionState>();
+}
+
 
 std::vector<std::pair<int,int>> MovementsKnight::getMovements(const std::shared_ptr<Knight> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard)
 {
@@ -7,43 +15,43 @@ std::vector<std::pair<int,int>> MovementsKnight::getMovements(const std::shared_
 
     //up-left
     std::pair<int,int> upLeft(figure->getPosition().first-1, figure->getPosition().second-2);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(upLeft);
+    pushPosition(movements, upLeft, figure, figuresOnBoard);
 
     //up-right
     std::pair<int,int> upRight(figure->getPosition().first+1, figure->getPosition().second-2);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(upRight);
+    pushPosition(movements, upRight, figure, figuresOnBoard);
 
     //right-up
     std::pair<int,int> rightUp(figure->getPosition().first+2, figure->getPosition().second-1);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(rightUp);
+    pushPosition(movements, rightUp, figure, figuresOnBoard);
 
     //right-down
     std::pair<int,int> rightDown(figure->getPosition().first+2, figure->getPosition().second+1);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(rightDown);
+    pushPosition(movements, rightDown, figure, figuresOnBoard);
 
     //down-right
     std::pair<int,int> downRight(figure->getPosition().first+1, figure->getPosition().second+2);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(downRight);
+    pushPosition(movements, downRight, figure, figuresOnBoard);
 
     //down-left
     std::pair<int,int> downLeft(figure->getPosition().first-1, figure->getPosition().second+2);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(downLeft);
+    pushPosition(movements, downLeft, figure, figuresOnBoard);
 
     //left-down
     std::pair<int,int> leftDown(figure->getPosition().first-2, figure->getPosition().second+1);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(leftDown);
+    pushPosition(movements, leftDown, figure, figuresOnBoard);
 
     //left-up
     std::pair<int,int> leftUp(figure->getPosition().first-2, figure->getPosition().second-1);
-    if(positionState.checkPositionState(figure,figuresOnBoard,upLeft).first==-1)
-        movements.push_back(leftUp);
+    pushPosition(movements, leftUp, figure, figuresOnBoard);
 
     return movements;
+}
+
+void MovementsKnight::pushPosition(std::vector<std::pair<int,int>> & movements, const std::pair<int,int> & newPosition, const std::shared_ptr<Knight> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard)
+{
+    if(movementsPositionState->positionExist(newPosition))
+        if(movementsPositionState->getFigureOnPosition(newPosition,figuresOnBoard)!=nullptr)
+            if(movementsPositionState->getFigureOnPosition(newPosition,figuresOnBoard)->getColor() != figure->getColor())
+                movements.push_back(newPosition);
 }
--- src/include/MovementsKnight.hpp
+++ src/include/MovementsKnight.hpp
@@ -1,14 +1,21 @@
 #pragma once
+#include <memory>
 #include <vector>
-#include "MovementsPositionState.hpp"
-#include "Knight.hpp"
 
+class MovementsPositionState;
+class Knight;
+class Figure;
 
 class MovementsKnight
 {
 public:
+    MovementsKnight();
     //Returns figure if the movements ends withempassant
     std::vector<std::pair<int,int>> getMovements(const std::shared_ptr<Knight> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
+
 private:
-    MovementsPositionState positionState;
+
+    void pushPosition(std::vector<std::pair<int,int>> & movements, const std::pair<int,int> & newPosition, const std::shared_ptr<Knight> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
+    std::shared_ptr<MovementsPositionState> movementsPositionState;
+
 };
