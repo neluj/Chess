0c81ca5e6bffb8ad0cada65121ec261147e11a74
Julen <juliantxo14@gmail.com>
2023-02-15 18:44:03 +0100

    Added figure checkers to position state class

--- src/MovementsPositionState.cpp
+++ src/MovementsPositionState.cpp
@@ -19,6 +19,34 @@ std::shared_ptr<Figure> MovementsPositionState::getFigureOnPosition(const std::p
     return figureOnPosition;
 }
 
+bool MovementsPositionState::isFree          (const std::pair<int, int> & position, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard)
+{
+    std::shared_ptr<Figure> figureOnPosition = getFigureOnPosition(position, figuresOnBoard);
+    if(figureOnPosition == nullptr)
+        return true;
+    return false;
+}
+
+bool MovementsPositionState::isEnemyFigureOnPosition(const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::pair<int,int> & calculatedPosition)
+{
+    std::shared_ptr<Figure> figureOnPosition = getFigureOnPosition(calculatedPosition, figuresOnBoard);
+    if(isFree(calculatedPosition, figuresOnBoard))
+        return false;
+    if(figure->getColor() != figureOnPosition->getColor())
+        return true;
+    return false;
+}
+bool MovementsPositionState::isTeamFigureOnPosition (const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::pair<int,int> & calculatedPosition)
+{
+    std::shared_ptr<Figure> figureOnPosition = getFigureOnPosition(calculatedPosition, figuresOnBoard);
+    if(isFree(calculatedPosition, figuresOnBoard))
+        return false;
+    if(figure->getColor() == figureOnPosition->getColor())
+        return true;
+    return false;
+}
+
+
 std::pair<int,int> MovementsPositionState::returnNullValue()
 {
     return std::pair<int, int>(-1,-1);
--- src/include/MovementsPositionState.hpp
+++ src/include/MovementsPositionState.hpp
@@ -7,12 +7,15 @@ class MovementsPositionState
 
 public:
 
-    // TODO borrar esta funcion
+    bool positionExist         (const std::pair<int, int>  &position);
+    // DELETE borrar esta funcion
     std::pair<int,int> checkPositionState(const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::pair<int,int> & calculatedPosition);
     std::shared_ptr<Figure> getFigureOnPosition(const std::pair<int, int> & position, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
-    bool positionExist(const std::pair<int, int>  &position);
-    
-private:
-    std::pair<int,int> returnNullValue();
+    bool isFree          (const std::pair<int, int> & position, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
+    bool isEnemyFigureOnPosition(const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::pair<int,int> & calculatedPosition);
+    bool isTeamFigureOnPosition (const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::pair<int,int> & calculatedPosition);
 
+ private:
+    // DELETE borrar esta funcion
+    std::pair<int,int> returnNullValue(); 
 };
