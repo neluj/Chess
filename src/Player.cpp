cef4c66bdf238d4698ebc1d46e99cdecfcf09b5e
Julen <juliantxo14@gmail.com>
2023-03-18 19:03:50 +0100

    Updated player with default name

--- src/Player.cpp
+++ src/Player.cpp
@@ -6,6 +6,18 @@ Player::Player(Figure::Color figureColor, std::string name)
 {
     this->asignedFigureColor        = figureColor;
     this->name                      = name;
+    playerCounter++;
+}
+
+Player::Player(Figure::Color figureColor)
+{
+    this->asignedFigureColor        = figureColor;
+
+    std::string DEFAULT_PLAYER_NAME = "Player " + std::to_string(playerCounter); 
+
+    this->name = DEFAULT_PLAYER_NAME;
+    playerCounter++;
+
 }
 
 std::string Player::getName()
--- src/include/Player.hpp
+++ src/include/Player.hpp
@@ -6,13 +6,13 @@ namespace chess
 {
 
 static size_t playerCounter = 0;
-std::string DEFAULT_PLAYER_NAME = "Player " + std::to_string(playerCounter); 
 
 class Player 
 {
 public:
-    
-    Player(Figure::Color figureColor, std::string name = DEFAULT_PLAYER_NAME); 
+
+    Player(Figure::Color figureColor, std::string name); 
+    Player(Figure::Color figureColor); 
 
     std::string getName();
     Figure::Color getAsignedFigureColor();
@@ -21,6 +21,7 @@ private:
 
         std::string name;
         Figure::Color asignedFigureColor;
+
 };
 
 }
