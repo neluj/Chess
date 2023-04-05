ad58cdf1ee58df83d7606debc3e10f31a80725cc
Julen <juliantxo14@gmail.com>
2023-04-06 00:51:01 +0200

    Defined Figure Mock

---
+++ test/Mocks/FigureMock.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "Figure.hpp"
+
+using namespace chess;
+
+class FigureMock : public Figure
+{   
+public:
+    MOCK_METHOD((std::vector<std::shared_ptr<std::pair<int,int>>>),   getPossibleMovements,   
+    (const std::vector<std::shared_ptr<Figure>> &),  (override)  );
+
+    MOCK_METHOD(std::shared_ptr<Figure>,                            clone,                  
+    (),                                                             (const, override)  );
+
+    MOCK_METHOD(void,                                               updatePosition,         
+    (int,int)                                                                  );
+
+    MOCK_METHOD((const std::shared_ptr<const std::pair<int, int>>),   getPosition,            
+    (),                                                             (const)     );
+
+    MOCK_METHOD(Type,                                               getType,                
+    (),                                                             (const)     );
+    
+    MOCK_METHOD(Color,                                              getColor,               
+    (),                                                             (const)     );
+};
