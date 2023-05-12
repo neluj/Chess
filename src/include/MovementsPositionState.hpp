6cfcebc3d6b291934451ff5117d5496dbeed6a93
Julen <juliantxo14@gmail.com>
2023-05-12 14:01:04 +0200

    Refactoriced on deep template

--- src/include/MovementsPositionState.hpp
+++ src/include/MovementsPositionState.hpp
@@ -13,8 +13,8 @@ public:
     bool positionExist         (const std::shared_ptr<std::pair<int,int>>  &position);
     std::shared_ptr<Figure> getFigureOnPosition(const std::shared_ptr<std::pair<int,int>> & position, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
     bool isFree          (const std::shared_ptr<std::pair<int,int>> & position, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard);
-    bool isEnemyFigureOnPosition(const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::shared_ptr<std::pair<int,int>> & calculatedPosition);
-    bool isTeamFigureOnPosition (const std::shared_ptr<Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::shared_ptr<std::pair<int,int>> & calculatedPosition);
+    bool isEnemyFigureOnPosition(const std::shared_ptr<const Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::shared_ptr<std::pair<int,int>> & calculatedPosition);
+    bool isTeamFigureOnPosition (const std::shared_ptr<const Figure> figure, const std::vector<std::shared_ptr<Figure>> & figuresOnBoard, const std::shared_ptr<std::pair<int,int>> & calculatedPosition);
 
 };
 
